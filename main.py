e832c9c119fbb76c9f783806827bdd8018f78640
TamarGamble1 <97067014+tamargamble@users.noreply.github.com>
2022-01-04 23:18:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+print('Welcome to the Autocorrect Program')
+name=input('What is your name?')
+print(f'Welcome {name}')
+
+print('I am here to help you fix any mistakes in spelling or grammar made while you may be typing.')
+
+user = input('Are you ready to begin?')
+if user == ('yes'):
+  print("You May Continue.")
+if user ==('no'):
+  print("Goodbye")
+
+print('Enter any text, or click "quit" to exit the program')
+
+
+if user == ('sng'):
+  print("sing")
+if user == ('My daf liks my brothei more than mh'):
+  print == ("My dad likes my brother more than me")
+if user == ('jewlry'):
+  print("jewelry")
+
